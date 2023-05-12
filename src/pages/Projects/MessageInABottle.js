b0350340d28b44159afb7972c681869a55757d4c
George Atkinson <georgeatkinsonwd@gmail.com>
2023-05-12 19:34:57 +0100

    added poem css

--- src/pages/Projects/MessageInABottle.js
+++ src/pages/Projects/MessageInABottle.js
@@ -5,20 +5,20 @@ function MessageInABottle() {
     <div className="projectContainer">
       <div className="projectText">
         <h1 className="projectTitle">Message in a bottle</h1>
-        <p className="projectDetails">
-        Formed by the mother
-        Tranquil and pure
-        Held in her cradle our worries dismissed
-        She rhythmically sways
-        A hypnotic beauty
-        A constant beating heart
-        A mystical home delicate yet forgotten
-        Neglected, littered, suffocated
-        But she is a powerful force that shall not be ignored
-        Growing, fighting, swelling
+        
+        <span className="poem">Formed by the mother</span>
+        <span className="poem">Tranquil and pure</span>
+        <span className="poem">Held in her cradle our worries dismissed</span>
+        <span className="poem">She rhythmically sways</span>
+        <span className="poem">A hypnotic beauty</span>
+        <span className="poem">A constant beating heart</span>
+        <span className="poem">A mystical home delicate yet forgotten</span>
+        <span className="poem">Neglected, littered, suffocated</span>
+        <span className="poem">But she is a powerful force that shall not be ignored</span>
+        <span className="poem">Growing, fighting, swelling</span>
 
-        Sending a message
-        </p>
+        <span className="poem">Sending a message</span>
+        
         </div>
         <img className="projectImage" src="https://res.cloudinary.com/dsaznciea/image/upload/v1683036198/Message%20in%20a%20Bottle/Picture10_lbqa40.jpg" alt="bottle 1" />
         <img className="projectImage" src="https://res.cloudinary.com/dsaznciea/image/upload/v1683036197/Message%20in%20a%20Bottle/Picture7_br8cpw.png" alt="bottle 2" />
